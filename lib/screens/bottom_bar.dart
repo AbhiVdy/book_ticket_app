99a73acaca7db84b7a1ae9e44093d85e1188cae1
AbhijeetV <Abhijeet.Vaidya@warbabank.com>
2022-11-30 19:05:47 +0530

    initial commit

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Ticket"),
+      ),
+      body: const Center(
+        child: Text("App Body"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.teal,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplane_ticket),
+            label: "Ticket",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
